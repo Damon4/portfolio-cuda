85ab3a975aedb9bc9a6b8d4dda346ac5b08aa322
Dmitry <sd@devtodev.com>
2022-02-06 19:34:15 +0300

    refactor pug templates, webp & avif support

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,12 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('work!');
+  document.querySelector('.jsHomeButtonWork').addEventListener('click', function (event) {
+    var _document$querySelect;
+
+    event.preventDefault();
+    event.stopPropagation();
+    (_document$querySelect = document.querySelector('#contact')) === null || _document$querySelect === void 0 ? void 0 : _document$querySelect.scrollIntoView();
+  });
 });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('work!')
+    document.querySelector('.jsHomeButtonWork').addEventListener('click', (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        document.querySelector('#contact')?.scrollIntoView()
+    })
 })
